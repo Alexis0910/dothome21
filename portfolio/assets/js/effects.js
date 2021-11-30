fcdac3c90cd2163f187c765847f43f0498efea4b
Alexis0910 <soeunpark1211@gmail.com>
2021-11-30 14:00:17 +0900

    1130

--- portfolio/assets/js/effects.js
+++ portfolio/assets/js/effects.js
@@ -1,4 +1,3 @@
-
 //공통 애니메이션
 $(".splice").each(function (index) {
     let text = $(this).text();
@@ -34,23 +33,56 @@ if ($(window).scrollTop() >= $(".splice").offset().top) {
 }
 
 // //window scroll function ** 
-// $(window).scroll(function () {
-//     let scrollTop = $(window).scrollTop();
+$(window).scroll(function () {
+    let wScroll = $(window).scrollTop();
+    console.log(wScroll);
+    // const mainPic = document.querySelector(".image .mainPic");
+    // const lake = document.querySelector(".image .lake");
 
-//     const mainPic = document.querySelector(".image .mainPic");
-//     const lake = document.querySelector(".image .lake");
+    //#about art2 list down to up
+    if (wScroll >= $(".divide").offset().top * 0.8) {
+        //divide.d1 애니메이션
+        TweenMax.to(".divide .list-desc span", 1, {
+            y: (0),
+            opacity: 1,
+            ease: Power4.edaseOut
+        });
+    }
+    //#about 20-21 effect right to left
+    if (wScroll >= $(".mText").offset().top * 0.9) {
+        //divide.d1 애니메이션
+        TweenMax.to(".mText .list-descR span", 1, {
+            x: (0),
+            opacity: 1,
+            ease: Power4.easeOut,
+            delay:0.3
+        });
+    }
 
-//     //gsap
-//     $("#About .article1 .title h1").each(function (index) {
-//         if ($(window).scrollTop() >= $(this).offset().top) {
-//             gsap.to($(this).find("strong span"), { //먼저 각  섹션을찾고 그안의 desc span을 find해라
-//                 duration: 0.1,
-//                 opacity: 1,
-//                 stagger: 0.05, //시간차를 주는 gsap기능(이러면 show붙여서 시간차setTimeout 이런거 할 필요가없지)
-//                 y: 0, //위에 css에서 translateY를 100을주고 여기서는 심플하게 y라고 하고 0 하면 애니메이션 차이
-//                 ease: "power4.out",
-//                 rotation: 0,
-//                 scale: 1
-//             });
-//         }
-//     });
+    //#about art4 mypro up to down
+    if (wScroll >= $("#About .article4 .scrollDown .text").offset().top*0.9) {
+        const load1 = document.querySelector("load1");
+        const load2 = document.querySelector("load2");
+        const load3 = document.querySelector("load3");
+
+        // var t2 = new TimelineLite()
+        // t2.staggerTo([load1, load2, load3], 1, {
+        //     y: (0),
+        //     opacity: 1,
+        //     ease: Power4.easeOut,
+        // }, 0.5);
+        // //divide.d1 애니메이션
+        TweenMax.to(".scrollDown .text .list-desc .load1 ", 1, {
+            y: (0), opacity: 1, ease: Power4.easeOut, delay:0.3,
+        });
+        TweenMax.to(".scrollDown .text .list-desc .load2 ", 1, {
+            y: (0), opacity: 1, ease: Power4.easeOut, delay:0.7,
+        });
+        TweenMax.to(".scrollDown .text .list-desc .load3 ", 1, {
+            y: (0), opacity: 1, ease: Power4.easeOut, delay:1,
+        });
+        TweenMax.to(".scrollDown .list-desc .scroll ", 1, {
+            y: (0), opacity: 1, ease: Power4.easeOut, delay:0.1,
+        });
+    }
+});
