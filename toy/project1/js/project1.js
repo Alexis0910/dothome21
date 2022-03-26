3881aa43c07bf720005c465209a85e06b6b4c5fb
Alexis0910 <soeunpark1211@gmail.com>
2022-03-26 11:50:55 +0900

    inputBox

--- toy/project1/js/project1.js
+++ toy/project1/js/project1.js
@@ -1,1 +1,3 @@
-document.querySelector()
+const addOrClose = document.querySelector(".addOrClose");
+const xbutton = document.querySelector(".xbutton");
+const trash = document.querySelector(".trash");
