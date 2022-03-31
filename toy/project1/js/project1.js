6533230c6155a2e05260b6cc0e9ee8876d30e881
Alexis0910 <soeunpark1211@gmail.com>
2022-03-31 16:54:54 +0900

    fit

--- toy/project1/js/project1.js
+++ toy/project1/js/project1.js
@@ -1,3 +1,16 @@
-const addOrClose = document.querySelector(".addOrClose");
-const xbutton = document.querySelector(".xbutton");
-const trash = document.querySelector(".trash");
+const addOrClose = document.querySelectorAll(".addOrClose");
+const xbutton = document.querySelectorAll(".xbutton");
+const trash = document.querySelectorAll(".trash");
+const list = document.querySelectorAll(".list");
+
+document.querySelector(".add").addEventListener("click", function(){
+
+})
+document.querySelector(".delete").addEventListener("click", function(){
+    document.querySelector(".delete").classList.add("active");
+    list.forEach((element, index)=>{
+        element.addEventListener("click",function(){
+            element.classList.toggle("active")
+        });
+    });
+})
